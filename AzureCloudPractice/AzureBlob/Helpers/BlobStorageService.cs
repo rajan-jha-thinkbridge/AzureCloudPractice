9b1d909aeac35521f16171d5b4c8d11da61f1c74
rajan-jha-thinkbridge <rajan.j@thinkbridge.in>
2023-11-28 10:00:12 +0530

    Created hepler class blob storage service to mange upload,download and delete file

---
+++ AzureCloudPractice/AzureBlob/Helpers/BlobStorageService.cs
@@ -0,0 +1,79 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlob.Helpers
+{
+    public class BlobStorageService
+    {
+        private BlobServiceClient _blobServiceClient;
+
+        public BlobStorageService(string connectionString)
+        {
+            _blobServiceClient = new BlobServiceClient(connectionString);
+        }
+
+        public async Task UploadFileToBlobAsync(string containerName, string blobName, string localFilePath)
+        {
+            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            Console.WriteLine("Uploading to Blob storage...");
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+
+            Console.WriteLine("File uploaded to Blob storage.");
+        }
+
+        public async Task DownloadBlobToFileAsync(string containerName, string blobName, string downloadFilePath)
+        {
+            try
+            {
+                BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+                BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+                Console.WriteLine("Downloading from Blob storage...");
+                BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+                using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+                {
+                    await download.Content.CopyToAsync(downloadFileStream);
+                }
+
+                Console.WriteLine("File downloaded from Blob storage.");
+            }
+            catch (RequestFailedException ex)
+            {
+                Console.WriteLine($"Error downloading blob: {ex.Message}");
+                // Handle exception as needed
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                // Handle exception as needed
+            }
+        }
+
+        public async Task ListBlobsInContainerAsync(string containerName)
+        {
+            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine($"Blob: {blobItem.Name}");
+            }
+        }
+
+        public async Task DeleteBlobAsync(string containerName, string blobName)
+        {
+            BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            await blobClient.DeleteIfExistsAsync();
+            Console.WriteLine("Blob deleted from Blob storage.");
+        }
+    }
+}
