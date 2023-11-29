94be4b38fe288ef73cba9d25842d38ab0439c5f5
rajan-jha-thinkbridge <rajan.j@thinkbridge.in>
2023-11-29 10:00:11 +0530

    Migration file

---
+++ AzureCloudPractice/EFCoreCrudApplication/Migrations/20231129042122_InitialEmployeeDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreCrudApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialEmployeeDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresse",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    AddressDetails = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresse", x => x.AddressId);
+                    table.ForeignKey(
+                        name: "FK_Addresse_employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresse_EmployeeId",
+                table: "Addresse",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresse");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
