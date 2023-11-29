dadabd605763b534715fc6480e2b51f76af58880
rajan-jha-thinkbridge <rajan.j@thinkbridge.in>
2023-11-29 09:59:56 +0530

    Created DbContext For migration

---
+++ AzureCloudPractice/EFCoreCrudApplication/Dbcontext/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCoreCrudApplication.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCrudApplication.Dbcontext
+{
+    public class EmployeeDbContext :DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+
+        public DbSet<Address> Addresse { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-HCF92PN\SQLEXPRESS01;Initial Catalog=EmployeeData;Integrated Security=True;Encrypt=False;");
+        }
+    }
+}
