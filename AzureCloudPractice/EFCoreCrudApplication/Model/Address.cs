35adc953cf0bbcc69a7cd18633b20849bb9a734e
rajan-jha-thinkbridge <rajan.j@thinkbridge.in>
2023-11-29 09:58:30 +0530

    Created Model For Employee And Address

---
+++ AzureCloudPractice/EFCoreCrudApplication/Model/Address.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace EFCoreCrudApplication.Model
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+
+        public int EmployeeId { get; set; } // Foreign key to link to Employee
+
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; } // Navigation property to access the related Employee
+
+        public string AddressDetails { get; set; } // New field to store the address details
+    }
+
+}
---
+++ AzureCloudPractice/EFCoreCrudApplication/Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreCrudApplication.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Address> Addresses { get; set; } // One-to-many relationship with Address
+
+    }
+}
