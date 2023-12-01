cda4176ed420eca4ebfc2ac7db6c6373f9f5f74a
rajan-jha-thinkbridge <rajan.j@thinkbridge.in>
2023-12-01 15:57:08 +0530

    Added console app to call Azure Function locally

---
+++ AzureCloudPractice/AzureFunctionConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var httpClient = new HttpClient();
+        var functionUrl = "http://localhost:7071/api/MyHttpTrigger";
+
+        Console.WriteLine("Enter your name:");
+        var name = Console.ReadLine();
+
+        var response = await httpClient.GetAsync($"{functionUrl}?name={name}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine($"Failed to call the function. Status code: {response.StatusCode}");
+        }
+
+        httpClient.Dispose();
+    }
+}
